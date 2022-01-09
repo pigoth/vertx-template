2e65ad19544afe24b81a606a393b589767452f8c
Thomas Ferrari <thomas.ferrari@cherrychain.it>
2022-01-09 16:06:18 +0100

    create base template to start quickly with vert.x application

---
+++ src/main/java/org/example/template/MainVerticle.kt
@@ -0,0 +1,36 @@
+package org.example.template
+
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import io.vertx.core.AbstractVerticle
+import io.vertx.core.Promise
+import io.vertx.core.json.jackson.DatabindCodec
+import io.vertx.ext.web.Router
+import org.slf4j.LoggerFactory
+
+class MainVerticle : AbstractVerticle() {
+
+  private val log = LoggerFactory.getLogger(javaClass)
+
+  override fun start(bootstrap: Promise<Void>) {
+    loadMapperModules()
+
+    val router = Router.router(vertx)
+
+    vertx.createHttpServer()
+      .requestHandler(router)
+      .listen(8080)
+      .onSuccess { server ->
+        log.info("HTTP server started on port " + server.actualPort())
+        bootstrap.complete()
+      }
+      .onFailure {
+        log.error("failed starting vertx", it)
+        bootstrap.fail(it)
+      }
+  }
+}
+
+fun loadMapperModules() {
+  DatabindCodec.mapper()
+    .registerKotlinModule()
+}
